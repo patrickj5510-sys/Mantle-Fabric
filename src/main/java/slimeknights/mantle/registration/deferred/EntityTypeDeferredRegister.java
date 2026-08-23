5ffbb1ed712ee04c3d93458d46530ded3ea1f2f3
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 16:19:35 -0400

    Port entity deferred register to DeferredHolder

--- src/main/java/slimeknights/mantle/registration/deferred/EntityTypeDeferredRegister.java
+++ src/main/java/slimeknights/mantle/registration/deferred/EntityTypeDeferredRegister.java
@@ -1,28 +1,23 @@
 package slimeknights.mantle.registration.deferred;
 
-import io.github.fabricators_of_create.porting_lib.util.LazyRegistrar;
-import io.github.fabricators_of_create.porting_lib.util.LazySpawnEggItem;
-import io.github.fabricators_of_create.porting_lib.util.RegistryObject;
+import io.github.fabricators_of_create.porting_lib.registry.DeferredHolder;
+import io.github.fabricators_of_create.porting_lib.util.DeferredSpawnEggItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import slimeknights.mantle.registration.ItemProperties;
 
 import java.util.function.Supplier;
 
-/**
- * Deferred register for an entity, building the type from a builder instance and adding an egg
- */
+/** Deferred register for entity types, with optional spawn eggs. */
 @SuppressWarnings("unused")
 public class EntityTypeDeferredRegister extends DeferredRegisterWrapper<EntityType<?>> {
-
   private final SynchronizedDeferredRegister<Item> itemRegistry;
+
   public EntityTypeDeferredRegister(String modID) {
     super(Registries.ENTITY_TYPE, modID);
     itemRegistry = SynchronizedDeferredRegister.create(Registries.ITEM, modID);
@@ -34,29 +29,15 @@ public class EntityTypeDeferredRegister extends DeferredRegisterWrapper<EntityTy
     itemRegistry.register();
   }
 
-  /**
-   * Registers a entity type for the given entity type builder
-   * @param name  Entity name
-   * @param sup   Entity builder instance
-   * @param <T>   Entity class type
-   * @return  Entity registry object
-   */
-  public <T extends Entity> RegistryObject<EntityType<T>> register(String name, Supplier<FabricEntityTypeBuilder<T>> sup) {
-    return register.register(name, () -> sup.get().build());
+  /** Registers an entity type from a Fabric entity type builder. */
+  public <T extends Entity> DeferredHolder<EntityType<?>, EntityType<T>> register(String name, Supplier<FabricEntityTypeBuilder<T>> supplier) {
+    return register.register(name, () -> supplier.get().build());
   }
 
-  /**
-   * Registers a entity type for the given entity type builder, and registers a spawn egg for it
-   * @param name       Entity name
-   * @param sup        Entity builder instance
-   * @param primary    Primary egg color
-   * @param secondary  Secondary egg color
-   * @param <T>   Entity class type
-   * @return  Entity registry object
-   */
-  public <T extends Mob> RegistryObject<EntityType<T>> registerWithEgg(String name, Supplier<FabricEntityTypeBuilder<T>> sup, int primary, int secondary) {
-    RegistryObject<EntityType<T>> object = register(name, sup);
-    var spawnEgg = itemRegistry.register(name + "_spawn_egg", () -> new LazySpawnEggItem(object, primary, secondary, new Item.Properties()));
+  /** Registers an entity type and a deferred spawn egg for it. */
+  public <T extends Mob> DeferredHolder<EntityType<?>, EntityType<T>> registerWithEgg(String name, Supplier<FabricEntityTypeBuilder<T>> supplier, int primary, int secondary) {
+    DeferredHolder<EntityType<?>, EntityType<T>> object = register(name, supplier);
+    var spawnEgg = itemRegistry.register(name + "_spawn_egg", () -> new DeferredSpawnEggItem(object, primary, secondary, new Item.Properties()));
     ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.SPAWN_EGGS).register(entries -> entries.accept(spawnEgg.get()));
     return object;
   }
