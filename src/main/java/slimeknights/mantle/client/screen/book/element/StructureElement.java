23e0c4d93583e23320ebd6eb87151f4920acae75
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 13:33:21 -0400

    Port book structure renderer to Minecraft 1.21 JOML and GuiGraphics

--- src/main/java/slimeknights/mantle/client/screen/book/element/StructureElement.java
+++ src/main/java/slimeknights/mantle/client/screen/book/element/StructureElement.java
@@ -1,12 +1,11 @@
 package slimeknights.mantle.client.screen.book.element;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.math.Quaternion;
 import com.mojang.math.Transformation;
-import com.mojang.math.Vector3f;
+import io.github.fabricators_of_create.porting_lib.models.data.ModelData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
@@ -16,7 +15,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
-import net.minecraftforge.client.model.data.ModelData;
+import org.joml.AxisAngle4f;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import slimeknights.mantle.client.book.structure.StructureInfo;
 import slimeknights.mantle.client.book.structure.level.TemplateLevel;
 import slimeknights.mantle.client.render.MantleRenderTypes;
@@ -26,52 +27,43 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class StructureElement extends SizedBookElement {
-
   public boolean canTick = false;
-
   public float scale = 50f;
   public float transX = 0;
   public float transY = 0;
   public Transformation additionalTransform;
   public final StructureInfo renderInfo;
   public final TemplateLevel structureWorld;
-
   public long lastStep = -1;
   public long lastPrintedErrorTimeMs = -1;
 
   public StructureElement(int x, int y, int width, int height, StructureTemplate template, List<StructureTemplate.StructureBlockInfo> structure) {
     super(x, y, width, height);
-
     int[] size = {template.getSize().getX(), template.getSize().getY(), template.getSize().getZ()};
-
     this.scale = 100f / (float) IntStream.of(size).max().getAsInt();
-
     float sx = (float) width / (float) BookScreen.PAGE_WIDTH;
     float sy = (float) height / (float) BookScreen.PAGE_HEIGHT;
-
     this.scale *= Math.min(sx, sy);
-
     this.renderInfo = new StructureInfo(structure);
-
     this.structureWorld = new TemplateLevel(structure, renderInfo);
-
     this.transX = x + width / 2F;
     this.transY = y + height / 2F;
-
-    this.additionalTransform = new Transformation(null, new Quaternion(25, 0, 0, true), null, new Quaternion(0, -45, 0, true));
+    this.additionalTransform = new Transformation(null,
+      new Quaternionf().rotateYXZ(0, (float) (25 * Math.PI / 180f), 0), null,
+      new Quaternionf().rotateYXZ((float) (-45 * Math.PI / 180f), 0, 0));
   }
 
   @Override
-  public void draw(PoseStack transform, int mouseX, int mouseY, float partialTicks, Font fontRenderer) {
-    MultiBufferSource.BufferSource buffer = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
+  public void draw(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks, Font fontRenderer) {
+    MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
+    PoseStack transform = graphics.pose();
     PoseStack.Pose lastEntryBeforeTry = transform.last();
 
     try {
       long currentTime = System.currentTimeMillis();
-
-      if (this.lastStep < 0)
+      if (this.lastStep < 0) {
         this.lastStep = currentTime;
-      else if (this.canTick && currentTime - this.lastStep > 200) {
+      } else if (this.canTick && currentTime - this.lastStep > 200) {
         this.renderInfo.step();
         this.lastStep = currentTime;
       }
@@ -83,16 +75,12 @@ public class StructureElement extends SizedBookElement {
       int structureLength = this.renderInfo.structureLength;
       int structureWidth = this.renderInfo.structureWidth;
       int structureHeight = this.renderInfo.structureHeight;
-
       transform.pushPose();
-
       final BlockRenderDispatcher blockRender = Minecraft.getInstance().getBlockRenderer();
-
       transform.translate(this.transX, this.transY, Math.max(structureHeight, Math.max(structureWidth, structureLength)));
       transform.scale(this.scale, -this.scale, 1);
-      this.additionalTransform.push(transform);
-      transform.mulPose(new Quaternion(0, 0, 0, true));
-
+      transform.pushTransformation(this.additionalTransform);
+      transform.mulPose(new Quaternionf().rotateYXZ(0, 0, 0));
       transform.translate(structureLength / -2f, structureHeight / -2f, structureWidth / -2f);
 
       for (int h = 0; h < structureHeight; h++) {
@@ -100,34 +88,23 @@ public class StructureElement extends SizedBookElement {
           for (int w = 0; w < structureWidth; w++) {
             BlockPos pos = new BlockPos(l, h, w);
             BlockState state = this.structureWorld.getBlockState(pos);
-
             if (!state.isAir()) {
               transform.pushPose();
               transform.translate(l, h, w);
-
-              int overlay;
-
-              if (pos.equals(new BlockPos(1, 1, 1)))
-                overlay = OverlayTexture.pack(0, true);
-              else
-                overlay = OverlayTexture.NO_OVERLAY;
-
+              int overlay = pos.equals(new BlockPos(1, 1, 1)) ? OverlayTexture.pack(0, true) : OverlayTexture.NO_OVERLAY;
               ModelData modelData = ModelData.EMPTY;
               BlockEntity te = structureWorld.getBlockEntity(pos);
-
               if (te != null) {
                 modelData = te.getModelData();
               }
 
-              // TODO: verify that we should be using all types here
               BakedModel model = blockRender.getBlockModel(state);
               for (RenderType renderType : model.getRenderTypes(state, structureWorld.random, modelData)) {
                 blockRender.getModelRenderer().tesselateBlock(
-                  structureWorld, blockRender.getBlockModel(state), state, pos, transform,
-                  buffer.getBuffer(MantleRenderTypes.TRANSLUCENT_FULLBRIGHT), false, structureWorld.random, state.getSeed(pos),
-                  overlay, modelData, renderType);
+                  structureWorld, model, state, pos, transform,
+                  buffer.getBuffer(MantleRenderTypes.TRANSLUCENT_FULLBRIGHT), false, structureWorld.random,
+                  state.getSeed(pos), overlay, modelData, renderType);
               }
-
               transform.popPose();
             }
           }
@@ -136,19 +113,16 @@ public class StructureElement extends SizedBookElement {
 
       transform.popPose();
       transform.popPose();
-
     } catch (Exception e) {
       final long now = System.currentTimeMillis();
-
       if (now > this.lastPrintedErrorTimeMs + 1000) {
         e.printStackTrace();
         this.lastPrintedErrorTimeMs = now;
       }
-
-      while (lastEntryBeforeTry != transform.last())
+      while (lastEntryBeforeTry != transform.last()) {
         transform.popPose();
+      }
     }
-
     buffer.endBatch();
   }
 
@@ -171,11 +145,12 @@ public class StructureElement extends SizedBookElement {
 
   private Transformation forRotation(double rX, double rY) {
     Vector3f axis = new Vector3f((float) rY, (float) rX, 0);
-    float angle = (float) Math.sqrt(axis.dot(axis));
-
-    if (!axis.normalize())
+    float dot = axis.dot(axis);
+    if (dot < Float.MIN_NORMAL) {
       return Transformation.identity();
-
-    return new Transformation(null, new Quaternion(axis, angle, true), null, null);
+    }
+    float angle = (float) (Math.sqrt(dot) * Math.PI / 180f);
+    axis.normalize();
+    return new Transformation(null, new Quaternionf(new AxisAngle4f(angle, axis)), null, null);
   }
 }
