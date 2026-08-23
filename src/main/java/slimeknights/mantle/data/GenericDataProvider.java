501a39b08a78ee4e883c03e43fa34dda5e8aeaef
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 13:58:13 -0400

    Fix GenericDataProvider for Fabric datagen 1.21

--- src/main/java/slimeknights/mantle/data/GenericDataProvider.java
+++ src/main/java/slimeknights/mantle/data/GenericDataProvider.java
@@ -8,6 +8,7 @@ import com.google.gson.stream.JsonWriter;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.JsonOps;
 import lombok.RequiredArgsConstructor;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.resources.ResourceLocation;
@@ -26,7 +27,7 @@ import java.nio.file.Paths;
 import java.util.Comparator;
 
 /** Generic logic to convert any serializable object into JSON. */
-@SuppressWarnings({"unused", "SameParameterValue"})  // API
+@SuppressWarnings({"unused", "SameParameterValue"})
 @RequiredArgsConstructor
 public abstract class GenericDataProvider implements DataProvider {
   protected final FabricDataOutput output;
@@ -38,53 +39,32 @@ public abstract class GenericDataProvider implements DataProvider {
     this(output, type, folder, JsonHelper.DEFAULT_GSON);
   }
 
-  /**
-   * Saves the given object to JSON
-   * @param output         Output for writing
-   * @param location       Location relative to this data provider's root
-   * @param object         Object to save, will be converted using this provider's GSON instance
-   * @param keyComparator  Key comparator to use
-   */
   protected void saveJson(CachedOutput output, ResourceLocation location, Object object, @Nullable Comparator<String> keyComparator) {
     try {
-      Path path = this.generator.getOutputFolder().resolve(Paths.get(type.getDirectory(), location.getNamespace(), folder, location.getPath() + ".json"));
+      Path path = this.output.getOutputFolder().resolve(Paths.get(type.getDirectory(), location.getNamespace(), folder, location.getPath() + ".json"));
       saveStable(output, gson.toJsonTree(object), path, keyComparator);
     } catch (IOException e) {
       Mantle.logger.error("Couldn't create data for {}", location, e);
     }
   }
 
-  /**
-   * Saves the given object to JSON
-   * @param output     Output for writing
-   * @param location   Location relative to this data provider's root
-   * @param object     Object to save, will be converted using this provider's GSON instance
-   */
   protected void saveJson(CachedOutput output, ResourceLocation location, Object object) {
     saveJson(output, location, object, DataProvider.KEY_COMPARATOR);
   }
 
-  /**
-   * Saves the given object to JSON using a codec
-   * @param output     Output for writing
-   * @param location   Location relative to this data provider's root
-   * @param codec      Codec to save the object
-   * @param object     Object to save, will be converted using the passed codec
-   */
   protected <T> void saveJson(CachedOutput output, ResourceLocation location, Codec<T> codec, T object) {
     saveJson(output, location, codec.encodeStart(JsonOps.INSTANCE, object).getOrThrow(false, Mantle.logger::error));
   }
 
-  /** Recreation of {@link DataProvider#saveStable(CachedOutput, JsonElement, Path)} that allows swapping tke key comparator */
   @SuppressWarnings("UnstableApiUsage")
-  static void saveStable(CachedOutput cache, JsonElement pJson, Path pPath, @Nullable Comparator<String> keyComparator) throws IOException {
+  static void saveStable(CachedOutput cache, JsonElement json, Path path, @Nullable Comparator<String> keyComparator) throws IOException {
     ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
     HashingOutputStream hashingOutput = new HashingOutputStream(Hashing.sha1(), byteOutput);
     JsonWriter writer = new JsonWriter(new OutputStreamWriter(hashingOutput, StandardCharsets.UTF_8));
     writer.setSerializeNulls(false);
     writer.setIndent("  ");
-    GsonHelper.writeValue(writer, pJson, keyComparator);
+    GsonHelper.writeValue(writer, json, keyComparator);
     writer.close();
-    cache.writeIfNeeded(pPath, byteOutput.toByteArray(), hashingOutput.hash());
+    cache.writeIfNeeded(path, byteOutput.toByteArray(), hashingOutput.hash());
   }
 }
