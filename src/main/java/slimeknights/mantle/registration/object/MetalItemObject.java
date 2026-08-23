ddb6adb94a9c6b282e4b20a0b74eedd3ea176fd9
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 16:22:37 -0400

    Port metal item tag IDs to 1.21

--- src/main/java/slimeknights/mantle/registration/object/MetalItemObject.java
+++ src/main/java/slimeknights/mantle/registration/object/MetalItemObject.java
@@ -9,45 +9,29 @@ import net.minecraft.world.level.block.Block;
 
 import java.util.function.Supplier;
 
-/** Object wrapper containing ingots, nuggets, and blocks */
+/** Object wrapper containing ingots, nuggets, and blocks. */
 public class MetalItemObject extends ItemObject<Block> {
   private final Supplier<? extends Item> ingot;
   private final Supplier<? extends Item> nugget;
-  @Getter
-  private final TagKey<Block> blockTag;
-  @Getter
-  private final TagKey<Item> blockItemTag;
-  @Getter
-  private final TagKey<Item> ingotTag;
-  @Getter
-  private final TagKey<Item> nuggetTag;
+  @Getter private final TagKey<Block> blockTag;
+  @Getter private final TagKey<Item> blockItemTag;
+  @Getter private final TagKey<Item> ingotTag;
+  @Getter private final TagKey<Item> nuggetTag;
 
   public MetalItemObject(String tagName, ItemObject<? extends Block> block, Supplier<? extends Item> ingot, Supplier<? extends Item> nugget) {
     super(block);
     this.ingot = ingot;
     this.nugget = nugget;
-    this.blockTag = TagKey.create(Registries.BLOCK, new ResourceLocation("c", tagName + "_blocks"));
+    this.blockTag = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", tagName + "_blocks"));
     this.blockItemTag = getTag(tagName + "_blocks");
     this.ingotTag = getTag(tagName + "_ingots");
     this.nuggetTag = getTag(tagName + "_nuggets");
   }
 
-  /** Gets the ingot for this object */
-  public Item getIngot() {
-    return ingot.get();
-  }
+  public Item getIngot() { return ingot.get(); }
+  public Item getNugget() { return nugget.get(); }
 
-  /** Gets the ingot for this object */
-  public Item getNugget() {
-    return nugget.get();
-  }
-
-  /**
-   * Creates a tag for a resource
-   * @param name  Tag name
-   * @return  Tag
-   */
   private static TagKey<Item> getTag(String name) {
-    return TagKey.create(Registries.ITEM, new ResourceLocation("c", name));
+    return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", name));
   }
 }
