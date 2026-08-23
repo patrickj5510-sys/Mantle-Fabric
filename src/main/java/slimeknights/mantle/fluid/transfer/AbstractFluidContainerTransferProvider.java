8132c015d5ac23eeb04f4ab2902f9794dff9d04d
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 16:14:18 -0400

    Port fluid transfer datagen conditions to Fabric 1.21

--- src/main/java/slimeknights/mantle/fluid/transfer/AbstractFluidContainerTransferProvider.java
+++ src/main/java/slimeknights/mantle/fluid/transfer/AbstractFluidContainerTransferProvider.java
@@ -2,12 +2,12 @@ package slimeknights.mantle.fluid.transfer;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.mojang.serialization.JsonOps;
 import io.github.fabricators_of_create.porting_lib.fluids.FluidStack;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.resource.conditions.v1.ConditionJsonProvider;
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceCondition;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.data.CachedOutput;
-import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.tags.TagKey;
@@ -18,9 +18,7 @@ import slimeknights.mantle.data.GenericDataProvider;
 import slimeknights.mantle.recipe.helper.ItemOutput;
 import slimeknights.mantle.recipe.ingredient.FluidIngredient;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -39,7 +37,7 @@ public abstract class AbstractFluidContainerTransferProvider extends GenericData
   protected abstract void addTransfers();
 
   /** Adds a transfer to be saved */
-  protected void addTransfer(ResourceLocation id, IFluidContainerTransfer transfer, ConditionJsonProvider... conditions) {
+  protected void addTransfer(ResourceLocation id, IFluidContainerTransfer transfer, ResourceCondition... conditions) {
     TransferJson previous = allTransfers.putIfAbsent(id, new TransferJson(transfer, conditions));
     if (previous != null) {
       throw new IllegalArgumentException("Duplicate fluid container transfer " + id);
@@ -47,19 +45,19 @@ public abstract class AbstractFluidContainerTransferProvider extends GenericData
   }
 
   /** Adds a transfer to be saved */
-  protected void addTransfer(String name, IFluidContainerTransfer transfer, ConditionJsonProvider... conditions) {
-    addTransfer(new ResourceLocation(modId, name), transfer, conditions);
+  protected void addTransfer(String name, IFluidContainerTransfer transfer, ResourceCondition... conditions) {
+    addTransfer(ResourceLocation.fromNamespaceAndPath(modId, name), transfer, conditions);
   }
 
   /** Adds generic fill and empty for a container */
-  protected void addFillEmpty(String prefix, ItemLike item, ItemLike container, Fluid fluid, TagKey<Fluid> tag, long amount, ConditionJsonProvider... conditions) {
-    addTransfer(prefix + "empty",  new EmptyFluidContainerTransfer(Ingredient.of(item), ItemOutput.fromItem(container), new FluidStack(fluid, amount)), conditions);
+  protected void addFillEmpty(String prefix, ItemLike item, ItemLike container, Fluid fluid, TagKey<Fluid> tag, long amount, ResourceCondition... conditions) {
+    addTransfer(prefix + "empty", new EmptyFluidContainerTransfer(Ingredient.of(item), ItemOutput.fromItem(container), new FluidStack(fluid, amount)), conditions);
     addTransfer(prefix + "fill", new FillFluidContainerTransfer(Ingredient.of(container), ItemOutput.fromItem(item), FluidIngredient.of(tag, amount)), conditions);
   }
 
-  /** Adds generic fill and empty for a container */
-  protected void addFillEmptyNBT(String prefix, ItemLike item, ItemLike container, Fluid fluid, TagKey<Fluid> tag, long amount, ConditionJsonProvider... conditions) {
-    addTransfer(prefix + "empty",  new EmptyFluidWithNBTTransfer(Ingredient.of(item), ItemOutput.fromItem(container), new FluidStack(fluid, amount)), conditions);
+  /** Adds generic fill and empty for a container while copying item data */
+  protected void addFillEmptyNBT(String prefix, ItemLike item, ItemLike container, Fluid fluid, TagKey<Fluid> tag, long amount, ResourceCondition... conditions) {
+    addTransfer(prefix + "empty", new EmptyFluidWithNBTTransfer(Ingredient.of(item), ItemOutput.fromItem(container), new FluidStack(fluid, amount)), conditions);
     addTransfer(prefix + "fill", new FillFluidWithNBTTransfer(Ingredient.of(container), ItemOutput.fromItem(item), FluidIngredient.of(tag, amount)), conditions);
   }
 
@@ -67,18 +65,21 @@ public abstract class AbstractFluidContainerTransferProvider extends GenericData
   public CompletableFuture<?> run(CachedOutput cache) {
     addTransfers();
     allTransfers.forEach((id, data) -> saveJson(cache, id, data.toJson()));
+    return CompletableFuture.completedFuture(null);
   }
 
-  /** Json with transfer and condition */
-  private record TransferJson(IFluidContainerTransfer transfer, ConditionJsonProvider[] conditions) {
-    /** Serializes this to JSON */
+  /** JSON wrapper for a transfer and any Fabric resource conditions. */
+  private record TransferJson(IFluidContainerTransfer transfer, ResourceCondition[] conditions) {
     private JsonElement toJson() {
       JsonElement element = FluidContainerTransferManager.GSON.toJsonTree(transfer, IFluidContainerTransfer.class);
-      assert element.isJsonObject();
+      if (!element.isJsonObject()) {
+        throw new IllegalStateException("Fluid transfer did not serialize to a JSON object");
+      }
       if (conditions.length != 0) {
         JsonArray array = new JsonArray();
-        for (ConditionJsonProvider condition : conditions) {
-          array.add(condition.toJson());
+        for (ResourceCondition condition : conditions) {
+          array.add(ResourceCondition.CODEC.encodeStart(JsonOps.INSTANCE, condition)
+            .getOrThrow(message -> new IllegalStateException("Failed to serialize resource condition: " + message)));
         }
         element.getAsJsonObject().add(ResourceConditions.CONDITIONS_KEY, array);
       }
