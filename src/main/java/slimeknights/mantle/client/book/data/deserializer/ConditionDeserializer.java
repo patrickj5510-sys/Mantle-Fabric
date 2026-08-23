3d671d1dc4b9857e4e49661ecbfbaa4d25bd1c08
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 13:31:05 -0400

    Decode book load conditions with Fabric 1.21 codecs

--- src/main/java/slimeknights/mantle/client/book/data/deserializer/ConditionDeserializer.java
+++ src/main/java/slimeknights/mantle/client/book/data/deserializer/ConditionDeserializer.java
@@ -1,22 +1,22 @@
 package slimeknights.mantle.client.book.data.deserializer;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-import net.fabricmc.fabric.api.resource.conditions.v1.ConditionJsonProvider;
+import com.mojang.serialization.JsonOps;
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceCondition;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 import slimeknights.mantle.client.book.data.JsonCondition;
 
 import java.lang.reflect.Type;
 
-/** Serializer for a forge condition */
+/** Serializer/deserializer for book resource conditions. */
 public interface ConditionDeserializer {
   ConditionDeserializer.Deserializer DESERIALIZER = new ConditionDeserializer.Deserializer();
   ConditionDeserializer.Serializer SERIALIZER = new ConditionDeserializer.Serializer();
@@ -24,26 +24,32 @@ public interface ConditionDeserializer {
   class Deserializer implements JsonDeserializer<JsonCondition> {
     @Override
     public JsonCondition deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
-      JsonObject jsonObject = GsonHelper.convertToJsonObject(json, ResourceConditions.CONDITIONS_KEY);
-      if (jsonObject.has(ResourceConditions.CONDITIONS_KEY)) {
-        JsonArray jsonCondition = jsonObject.getAsJsonArray(ResourceConditions.CONDITIONS_KEY);
-        ResourceLocation id = new ResourceLocation(jsonCondition.get(0).getAsJsonObject().get(ResourceConditions.CONDITION_ID_KEY).getAsString());
-        return new JsonCondition(id, jsonObject);
+      JsonObject jsonObject = GsonHelper.convertToJsonObject(json, "book condition");
+      if (!jsonObject.has(ResourceConditions.CONDITIONS_KEY)) {
+        return new JsonCondition();
       }
-      return new JsonCondition();
+
+      JsonElement encodedCondition = jsonObject.get(ResourceConditions.CONDITIONS_KEY);
+      ResourceCondition condition = ResourceCondition.CONDITION_CODEC.parse(JsonOps.INSTANCE, encodedCondition)
+        .resultOrPartial(message -> {})
+        .orElse(null);
+      return new JsonCondition(condition);
     }
   }
 
   class Serializer implements JsonSerializer<JsonCondition> {
     @Override
     public JsonElement serialize(JsonCondition src, Type typeOfSrc, JsonSerializationContext context) {
-      if (src.getConditionJsonProvider() != null) {
-        JsonObject conditions = new JsonObject();
-        ConditionJsonProvider.write(conditions, src.getConditionJsonProvider());
-        return conditions;
+      if (src.getCondition() == null) {
+        return JsonNull.INSTANCE;
       }
-      return null;
+
+      JsonElement encoded = ResourceCondition.CONDITION_CODEC.encodeStart(JsonOps.INSTANCE, src.getCondition())
+        .resultOrPartial(message -> {})
+        .orElse(JsonNull.INSTANCE);
+      JsonObject wrapper = new JsonObject();
+      wrapper.add(ResourceConditions.CONDITIONS_KEY, encoded);
+      return wrapper;
     }
   }
 }
-
