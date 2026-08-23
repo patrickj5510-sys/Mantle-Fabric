e10b26d5d55255a8aa6b42844e1ed6a5e4db7863
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 13:53:27 -0400

    Port Mantle render vertex formats to Minecraft 1.21

--- src/main/java/slimeknights/mantle/client/render/MantleRenderTypes.java
+++ src/main/java/slimeknights/mantle/client/render/MantleRenderTypes.java
@@ -1,6 +1,5 @@
 package slimeknights.mantle.client.render;
 
-import com.google.common.collect.ImmutableMap;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat.Mode;
@@ -9,40 +8,39 @@ import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import slimeknights.mantle.Mantle;
 
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.ELEMENT_COLOR;
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.ELEMENT_NORMAL;
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.ELEMENT_PADDING;
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.ELEMENT_POSITION;
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.ELEMENT_UV0;
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.ELEMENT_UV1;
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.ELEMENT_UV2;
-
-/**
- * Class for render types defined by Mantle
- */
+/** Class for render types defined by Mantle. */
 public class MantleRenderTypes extends RenderType {
-
-  private MantleRenderTypes(String name, VertexFormat format, Mode mode, int bufferSize, boolean useDelegate, boolean needsSorting, Runnable setupTaskIn, Runnable clearTaskIn) {
+  private MantleRenderTypes(String name, VertexFormat format, Mode mode, int bufferSize,
+                            boolean useDelegate, boolean needsSorting,
+                            Runnable setupTaskIn, Runnable clearTaskIn) {
     super(name, format, mode, bufferSize, useDelegate, needsSorting, setupTaskIn, clearTaskIn);
   }
 
-  /**
-   * Render type used for the fluid renderer
-   */
+  /** Fluid shader state. */
+  public static final RenderStateShard.ShaderStateShard FLUID_SHADER =
+    new RenderStateShard.ShaderStateShard(MantleShaders::getConfiguredFluidShader);
+
+  /** Render type used for the fluid renderer. */
   public static final RenderType FLUID = create(
     Mantle.modId + ":block_render_type",
     DefaultVertexFormat.POSITION_COLOR_TEX_LIGHTMAP, VertexFormat.Mode.QUADS, 256, false, true,
     RenderType.CompositeState.builder()
       .setLightmapState(LIGHTMAP)
-      .setShaderState(POSITION_COLOR_TEX_LIGHTMAP_SHADER)
+      .setShaderState(FLUID_SHADER)
       .setTextureState(BLOCK_SHEET_MIPPED)
       .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
       .createCompositeState(false));
 
-  /**
-   * Render type used for the structure renderer
-   */
-  public static final VertexFormat BLOCK_WITH_OVERLAY = new VertexFormat(ImmutableMap.<String, VertexFormatElement>builder().put("Position", ELEMENT_POSITION).put("Color", ELEMENT_COLOR).put("UV0", ELEMENT_UV0).put("UV1", ELEMENT_UV1).put("UV2", ELEMENT_UV2).put("Normal", ELEMENT_NORMAL).put("Padding", ELEMENT_PADDING).build());
+  /** Vertex format used for the structure renderer. */
+  public static final VertexFormat BLOCK_WITH_OVERLAY = VertexFormat.builder()
+    .add("Position", VertexFormatElement.POSITION)
+    .add("Color", VertexFormatElement.COLOR)
+    .add("UV0", VertexFormatElement.UV0)
+    .add("UV1", VertexFormatElement.UV1)
+    .add("UV2", VertexFormatElement.UV2)
+    .add("Normal", VertexFormatElement.NORMAL)
+    .padding(1)
+    .build();
 
   public static final RenderType TRANSLUCENT_FULLBRIGHT = create(
     Mantle.modId + ":translucent_fullbright",
