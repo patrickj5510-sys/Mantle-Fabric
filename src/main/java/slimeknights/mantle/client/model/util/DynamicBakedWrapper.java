ee41b1514d51b8ff1d4b53148c9e7bf5a870b1c1
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-23 13:53:02 -0400

    Clean dynamic baked wrapper for Fabric Renderer API 1.21

--- src/main/java/slimeknights/mantle/client/model/util/DynamicBakedWrapper.java
+++ src/main/java/slimeknights/mantle/client/model/util/DynamicBakedWrapper.java
@@ -1,24 +1,14 @@
 package slimeknights.mantle.client.model.util;
 
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.core.Direction;
-
 import net.fabricmc.fabric.api.renderer.v1.model.ForwardingBakedModel;
-
-import javax.annotation.Nullable;
-import java.util.List;
-import java.util.Random;
+import net.minecraft.client.resources.model.BakedModel;
 
 /**
- * Cross between {@link ForwardingBakedModel} and {@link net.minecraftforge.client.model.data.IDynamicBakedModel}.
- * Used to create a baked model wrapper that has a dynamic {@link #getQuads(BlockState, Direction, Random, IModelData)} without worrying about overriding the deprecated variant.
- * @param <T>  Baked model parent
+ * Base wrapper for Mantle models which supply dynamic Fabric Renderer API quad emission.
+ * @param <T> baked model parent type
  */
 @SuppressWarnings("WeakerAccess")
 public abstract class DynamicBakedWrapper<T extends BakedModel> extends ForwardingBakedModel {
-
   protected DynamicBakedWrapper(T originalModel) {
     wrapped = originalModel;
   }
