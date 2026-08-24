36010074305aac44bbf656eb46365c4705e28d75
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-24 12:44:55 -0400

    Port loaded condition context off Forge API

--- src/main/java/slimeknights/mantle/util/DataLoadedConditionContext.java
+++ src/main/java/slimeknights/mantle/util/DataLoadedConditionContext.java
@@ -6,7 +6,6 @@ import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraftforge.common.crafting.conditions.ICondition;
 
 import java.util.Collection;
 import java.util.Map;
@@ -15,10 +14,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /** Condition context to use when data has already been loaded, used in books for processing their conditions for instance. */
-public enum DataLoadedConditionContext implements ICondition.IContext {
+public enum DataLoadedConditionContext {
   INSTANCE;
 
-  @Override
   public <T> Collection<Holder<T>> getTag(TagKey<T> key) {
     Registry<T> registry = RegistryHelper.getRegistry(key.registry());
     if (registry != null) {
@@ -30,7 +28,6 @@ public enum DataLoadedConditionContext implements ICondition.IContext {
     return Set.of();
   }
 
-  @Override
   public <T> Map<ResourceLocation,Collection<Holder<T>>> getAllTags(ResourceKey<? extends Registry<T>> key) {
     Registry<T> registry = RegistryHelper.getRegistry(key);
     if (registry != null) {
