df3e8064eb97223aa175fc8a0fc271367891fd46
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-24 12:53:32 -0400

    Port nameable block entity serialization to 1.21

--- src/main/java/slimeknights/mantle/block/entity/NameableBlockEntity.java
+++ src/main/java/slimeknights/mantle/block/entity/NameableBlockEntity.java
@@ -3,43 +3,40 @@ package slimeknights.mantle.block.entity;
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
-/**
- * Extension of tile entity to make it namable
- */
+/** Extension of block entity to make it nameable. */
 public abstract class NameableBlockEntity extends MantleBlockEntity implements INameableMenuProvider {
-	private static final String TAG_CUSTOM_NAME = "CustomName";
+  private static final String TAG_CUSTOM_NAME = "CustomName";
 
-	/** Default title for this tile entity */
-	@Getter
-	private final Component defaultName;
-	/** Title set to this tile entity */
-	@Getter @Setter
-	private Component customName;
+  @Getter
+  private final Component defaultName;
+  @Getter @Setter
+  private Component customName;
 
-	public NameableBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state, Component defaultTitle) {
-		super(type, pos, state);
-		this.defaultName = defaultTitle;
-	}
+  public NameableBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state, Component defaultTitle) {
+    super(type, pos, state);
+    this.defaultName = defaultTitle;
+  }
 
-	@Override
-	public void load(CompoundTag tags) {
-		super.load(tags);
-		if (tags.contains(TAG_CUSTOM_NAME, Tag.TAG_STRING)) {
-			this.customName = Component.Serializer.fromJson(tags.getString(TAG_CUSTOM_NAME));
-		}
-	}
+  @Override
+  protected void loadAdditional(CompoundTag tags, HolderLookup.Provider registries) {
+    super.loadAdditional(tags, registries);
+    if (tags.contains(TAG_CUSTOM_NAME, Tag.TAG_STRING)) {
+      this.customName = Component.Serializer.fromJson(tags.getString(TAG_CUSTOM_NAME), registries);
+    }
+  }
 
-	@Override
-	public void saveSynced(CompoundTag tags) {
-		super.saveSynced(tags);
-		if (this.hasCustomName()) {
-			tags.putString(TAG_CUSTOM_NAME, Component.Serializer.toJson(this.customName));
-		}
-	}
+  @Override
+  protected void saveSynced(CompoundTag tags, HolderLookup.Provider registries) {
+    super.saveSynced(tags, registries);
+    if (this.hasCustomName()) {
+      tags.putString(TAG_CUSTOM_NAME, Component.Serializer.toJson(this.customName, registries));
+    }
+  }
 }
