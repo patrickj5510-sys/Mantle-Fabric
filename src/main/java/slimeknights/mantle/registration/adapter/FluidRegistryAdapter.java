a3cae044ca8a84319fd6e3533164a5a704b58033
patrickj5510-sys <patrickj5510@gmail.com>
2026-08-27 06:46:47 -0400

    Port fluid registry adapter to BaseFlowingFluid

--- src/main/java/slimeknights/mantle/registration/adapter/FluidRegistryAdapter.java
+++ src/main/java/slimeknights/mantle/registration/adapter/FluidRegistryAdapter.java
@@ -1,7 +1,7 @@
 package slimeknights.mantle.registration.adapter;
 
-import io.github.fabricators_of_create.porting_lib.util.SimpleFlowableFluid;
-import io.github.fabricators_of_create.porting_lib.util.SimpleFlowableFluid.Properties;
+import io.github.fabricators_of_create.porting_lib.fluids.BaseFlowingFluid;
+import io.github.fabricators_of_create.porting_lib.fluids.BaseFlowingFluid.Properties;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.material.Fluid;
 import slimeknights.mantle.registration.DelayedSupplier;
@@ -9,51 +9,33 @@ import slimeknights.mantle.registration.FluidBuilder;
 
 import java.util.function.Function;
 
-/**
- * Registry adapter for registering fluids
- */
+/** Registry adapter for registering fluids. */
 @SuppressWarnings("unused")
 public class FluidRegistryAdapter extends RegistryAdapter<Fluid> {
-  /** @inheritDoc */
   public FluidRegistryAdapter(Registry<Fluid> registry, String modId) {
     super(registry, modId);
   }
 
-  /**
-   * Registers a new fluid with both still and flowing variants
-   * @param builder   Fluid properties builder, contains the fluid type and possible bucket form
-   * @param still     Still constructor
-   * @param flowing   Flowing constructor
-   * @param name      Fluid name
-   * @param <F>       Fluid type
-   * @return  Still fluid instance
-   */
-  public <F extends SimpleFlowableFluid> F register(FluidBuilder<?> builder, Function<Properties, F> still, Function<Properties,F> flowing, String name) {
-    // have to create still and flowing later, as the props need these suppliers
+  /** Registers a new fluid with both source and flowing variants. */
+  public <F extends BaseFlowingFluid> F register(FluidBuilder<?> builder,
+                                                 Function<Properties,F> still,
+                                                 Function<Properties,F> flowing,
+                                                 String name) {
     DelayedSupplier<Fluid> stillDelayed = new DelayedSupplier<>();
     DelayedSupplier<Fluid> flowingDelayed = new DelayedSupplier<>();
 
-    // create props with the suppliers
     Properties props = builder.build(builder.getType(), stillDelayed, flowingDelayed);
 
-    // create fluids now that we have props
-    // TODO: should we be using holders?
     F stillFluid = register(still.apply(props), name);
     stillDelayed.setSupplier(() -> stillFluid);
     F flowingFluid = register(flowing.apply(props), "flowing_" + name);
     flowingDelayed.setSupplier(() -> flowingFluid);
 
-    // return the final nice object
     return stillFluid;
   }
 
-  /**
-   * Registers a fluid using default constructors
-   * @param builder  Fluid builder
-   * @param name     Fluid name
-   * @return  Still fluid
-   */
-  public SimpleFlowableFluid register(FluidBuilder<?> builder, String name) {
-    return register(builder, SimpleFlowableFluid.Still::new, SimpleFlowableFluid.Flowing::new, name);
+  /** Registers a fluid using Porting Lib's default 1.21 source/flowing implementations. */
+  public BaseFlowingFluid register(FluidBuilder<?> builder, String name) {
+    return register(builder, BaseFlowingFluid.Source::new, BaseFlowingFluid.Flowing::new, name);
   }
 }
